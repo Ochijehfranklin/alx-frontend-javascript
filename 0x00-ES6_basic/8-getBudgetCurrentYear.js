3a0636a3fd16bc620f3db35e5a1b42f17802f0ba
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-07-22 18:05:29 +0100

    ES6 method properties

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+  const budget = {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
+
+  return budget;
+}
