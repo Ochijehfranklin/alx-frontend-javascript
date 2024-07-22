4bced75f678d14d321203ff91fad3642ed8ce2be
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-07-22 18:53:56 +0100

    Iterator

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [departmentName]: employees,
+  };
+}
