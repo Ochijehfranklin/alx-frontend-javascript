c45cc1e085526e53bad550b7cfe246bd5bbfb0b8
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-07-22 16:53:04 +0100

    Block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    return [task, task2];
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
