977cb03e83135c2b73ed59f669f3f72d13eb623b
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-07-23 12:11:33 +0100

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+export default function getResponseFromAPI(){
+    const succeed = true;
+    return new Promise((resolve, reject) => {
+        if (succeed){
+            resolve("It was a success");
+        } else {
+            reject("it was not good");
+        }
+    });
+}
