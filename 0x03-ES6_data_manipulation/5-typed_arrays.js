1478520ef0d12379da305ea844238216414d8643
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-07-30 18:33:15 +0100

    Typed Array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,12 +3,11 @@
 
 export default function createInt8TypedArray(length, position, value){
     const buffer = new ArrayBuffer(length);
-    const int8View = new Int8Array(buffer);
 
     if (position < 0 || position >= length) {
         throw new Error('Position outside range');
     }
     const view = new DataView(buffer);
     view.setInt8(position, value)
-    return buffer;
+    return view;
 };
